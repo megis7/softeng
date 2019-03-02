8e5672c9247b53b694237f9830f803e333f05209
panoszogas <panoszogas2@gmail.com>
2019-03-02 02:22:41 +0200

    possible fix to edit-shop post

--- client/src/app/admin/edit-shop/edit-shop.component.ts
+++ client/src/app/admin/edit-shop/edit-shop.component.ts
@@ -26,15 +26,17 @@ export class EditShopComponent implements OnInit, AfterViewInit {
     lng: [''],
     tags: this.fb.array([this.fb.control('')])
   });
-
+  private currentLocation: Point= { 'lon': -1, 'lat': -1 };
   constructor(private route: ActivatedRoute, private shopService: ShopService, private fb: FormBuilder) { }
 
   ngOnInit() {
     this.route.params.subscribe(params => {
       const id = params["id"]||"0"
-
-      if(id == "0")
-        this.subscription = of(new Shop("0","","",0,0,[]))
+      this.getLocation();
+      
+      if(id == "0"){
+        this.subscription = of(new Shop("0","","",this.currentLocation.lon,this.currentLocation.lat,[]))
+      }
       else
         this.subscription = this.shopService.getShop(id)
 
@@ -55,8 +57,22 @@ export class EditShopComponent implements OnInit, AfterViewInit {
     })
   }
   ngAfterViewInit(){
-    this.mapDisplay.addPoint(new Point(this.activeShop.lng,this.activeShop.lat))
+    if(this.activeShop.id != "0")this.mapDisplay.addPoint(new Point(this.activeShop.lng,this.activeShop.lat))
   }
+
+  private canGetLocation(): boolean {
+		return navigator.geolocation != null;
+	}
+
+	private getLocation(): void {
+		if(navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(pos => {
+        this.currentLocation.lon = pos.coords.longitude;
+        this.currentLocation.lat =  pos.coords.latitude;
+			})
+		}
+	}
+
   displayMap(){
     console.log(this.mapDisplay.map.geocode(this.activeShop.address))
     console.log(this.activeShop.lat,this.activeShop.lng)
@@ -78,8 +94,12 @@ export class EditShopComponent implements OnInit, AfterViewInit {
   onSubmit(){
     const shop = this.shopForm.value
     console.log(shop)
-    if(shop.id == "0")
+    if(shop.id == "0"){
+      this.mapDisplay.addPoint(new Point(this.currentLocation.lon,this.currentLocation.lat))
+      this.activeShop.lng = this.currentLocation.lon
+      this.activeShop.lat = this.currentLocation.lat
       this.shopService.postShop(shop).subscribe(sho => {this.activeShop = sho;this.shopForm.setValue(this.activeShop)},err => console.log(err))
+    }
     else
       this.shopService.putShop(shop).subscribe(sho => {this.activeShop = sho;this.shopForm.setValue(this.activeShop)},err => console.log(err))
   }
